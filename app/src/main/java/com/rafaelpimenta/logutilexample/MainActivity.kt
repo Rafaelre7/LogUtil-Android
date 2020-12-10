a1d0f8b995e8ebfe16536e77cdb95f626e826bd5
Rafael Pimenta <rafaelre7@gmail.com>
2020-12-10 10:15:08 -0300

    Implementation gradle

--- app/src/main/java/com/rafaelpimenta/logutilexample/MainActivity.kt
+++ app/src/main/java/com/rafaelpimenta/logutilexample/MainActivity.kt
@@ -2,10 +2,27 @@ package com.rafaelpimenta.logutilexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.rafaelpimenta.logutil.LogDebug
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edtFrase: EditText
+    private lateinit var btnLog: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtFrase = findViewById(R.id.edtFrase)
+        btnLog = findViewById(R.id.btnLog)
+
+        btnLog.setOnClickListener {
+            LogDebug().print(edtFrase.text.toString() ?: "Sem dados")
+        }
+
     }
 }
--- logutil/src/main/java/com/rafaelpimenta/logutil/LogDebug.kt
+++ logutil/src/main/java/com/rafaelpimenta/logutil/LogDebug.kt
@@ -7,6 +7,6 @@ class LogDebug {
     private val TAG = "SUPER_AWESOME_APP"
 
     fun print(message: String) {
-        Log.d(TAG, message)
+        Log.e(TAG, message)
     }
 }
